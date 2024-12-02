d526335d1d42ba8ef0679b18b4dacd49d0cda5af
Tom Nicolosi <tom@teeehn.com>
2024-12-01 22:08:38 -0800

    Add web socket server and html file.

---
+++ index-ws.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const server = require('http').createServer();
+const app = express();
+
+app.get('/', function(req, res) {
+  res.sendFile('index.html', { root: __dirname });
+});
+
+server.on('request', app);
+server.listen(3000, function() {
+  console.log('Server listening on port 3000.');
+});
