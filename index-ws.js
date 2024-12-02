f5a1bd6b5bfa704179ce8d39e0624f85fec2c110
Tom Nicolosi <tom@teeehn.com>
2024-12-01 22:20:08 -0800

    Added ws ans web socket server.

--- index-ws.js
+++ index-ws.js
@@ -11,3 +11,31 @@ server.on('request', app);
 server.listen(3000, function() {
   console.log('Server listening on port 3000.');
 });
+
+// Begin Websockets
+
+const WebSocketServer = require('ws').Server;
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', function connection(ws) {
+  const numClients = wss.clients.size;
+  console.log('Client connected. Total connected clients: ' + numClients);
+
+  wss.broadcast('Current visitors: ' + numClients);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send('Welcome to the server!');
+  }
+
+  ws.on('close', function close() {
+    wss.broadcast('Current visitors: ' + numClients);
+    console.log('A client disconnected');
+  });
+});
+
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
